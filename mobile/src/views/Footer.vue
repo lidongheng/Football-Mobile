8dbe67ee777f7b8b0b136a83a8eb5d3766014ad3
lidongheng <huoyi-20@163.com>
2019-08-06 18:16:41 +0800

    移动端基础布局完毕

---
+++ mobile/src/views/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { State, Getter, Mutation, Action } from 'vuex-class'
+@Component({
+  components: {
+
+  }
+})
+export default class Footer extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ mobile/src/views/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { State, Getter, Mutation, Action } from 'vuex-class'
+@Component({
+  components: {
+
+  }
+})
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
