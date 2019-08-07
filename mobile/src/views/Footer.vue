484c713944d2bac8b42aacb0515bcd5d724370c0
lidongheng <huoyi-20@163.com>
2019-08-07 11:30:10 +0800

    完成header和footer

--- mobile/src/views/Footer.vue
+++ mobile/src/views/Footer.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="footer">
+    <ul>
+      <li>
+        <a href="">
+          <span class="iconfont">&#xe608;</span>
+          <p class="iconTitle">首页</p>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span class="iconfont">&#xe69a;</span>
+          <p class="iconTitle">球队</p>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span class="iconfont">&#xe63f;</span>
+          <p class="iconTitle">比赛</p>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span class="iconfont">&#xe62a;</span>
+          <p class="iconTitle">我</p>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,5 +41,30 @@ export default class Footer extends Vue {}
 </script>
 
 <style lang="scss">
-
+  @import url(../../public/css/font.css);
+  .footer {
+    height: 100%;
+    width: 100%;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  li {
+    width: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .iconfont {
+    font-size: 1rem;
+  }
+  .iconTitle {
+    font-size: .5866666rem;
+    text-align: center;
+  }
+  a {
+    color: #37A2D8;
+  }
 </style>
--- mobile/src/views/Header.vue
+++ mobile/src/views/Header.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="header">
+    <div class="back-wrapper">
+      <span class="iconfont">&#xe66a;</span>
+    </div>
+    <div class="title-wrapper">标题栏</div>
+    <div class="share-wrapper">
+      <span class="iconfont">&#xe64e;</span>
+    </div>
   </div>
 </template>
 
@@ -14,6 +21,30 @@ import { State, Getter, Mutation, Action } from 'vuex-class'
 export default class Header extends Vue {}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  @import url(../../public/css/font.css);
+  .header {
+    height: 1.36rem;
+    background: #37A2D8;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .back-wrapper,.share-wrapper {
+    height: 100%;
+    width: 1.1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .share-wrapper .iconfont {
+    font-size: .8rem;
+    padding-right: .21333333rem;
+  }
+  .iconfont {
+    font-size: .6666666rem;
+  }
+  .title-wrapper {
+    font-size: .5866666rem;
+  }
 </style>
