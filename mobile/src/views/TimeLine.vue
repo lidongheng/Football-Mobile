633e172454c6b1aa63e1e2294bb017c47bd34584
lidongheng <huoyi-20@163.com>
2019-08-12 16:31:05 +0800

    完成时间轴的样式设计

--- mobile/src/views/TimeLine.vue
+++ mobile/src/views/TimeLine.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="timeLine">
-    时间线页面
+    <ul>
+      <li>
+        <div class="left"></div>
+        <div class="center">
+          03-10
+        </div>
+        <div class="right">
+          感谢您在京东购物，欢迎您再次光临！感谢您在京东购物，欢迎您再次光临！
+          感谢您在京东购物，欢迎您再次光临！感谢您在京东购物，欢迎您再次光临！
+          感谢您在京东购物，欢迎您再次光临！感谢您在京东购物，欢迎您再次光临！
+          感谢您在京东购物，欢迎您再次光临！感谢您在京东购物，欢迎您再次光临！
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,5 +28,37 @@ export default class TimeLine extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-
+  .timeLine {
+    font-size: .426666rem;
+  }
+  ul {
+    white-space: nowrap;
+  }
+  li {
+    border-left: 1px solid #89c7ff;
+    margin-left: .2666666rem;
+  }
+  .left {
+    position: relative;
+    left: -.2rem;
+    width: 5%;
+    min-width: .4rem;
+    max-width: .4rem;
+    height: .4rem;
+    background-color: #fff;
+    border-radius: 50%;
+    border: .1066666rem solid #89c7ff;
+  }
+  .center {
+    width: 20%;
+    min-width: 1.466666rem;
+  }
+  .right {
+    width: 75%;
+    white-space: normal;
+  }
+  li div {
+    display: inline-block;
+    vertical-align: top;
+  }
 </style>
