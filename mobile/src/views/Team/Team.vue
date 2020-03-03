0fe0d73d0d9f1ca4442d12a0f35aaf4745170a5c
lidongheng <huoyi-20@163.com>
2020-03-03 11:37:52 +0800

    完整版

--- mobile/src/views/Team/Team.vue
+++ mobile/src/views/Team/Team.vue
@@ -194,6 +194,34 @@
                 <p>皇家社会</p>
               </router-link>
               </li>
+              <li><router-link to="/team/Valencia/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Laliga/Valencia.jpg')" alt=""/>
+                </div>
+                <p>瓦伦西亚</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/AtleticoMadrid/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Laliga/AtleticoMadrid.jpg')" alt=""/>
+                </div>
+                <p>马德里竞技</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Barcelona/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Laliga/Barcelona.jpg')" alt=""/>
+                </div>
+                <p>巴塞罗那</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/RealMadrid/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Laliga/RealMadrid.jpg')" alt=""/>
+                </div>
+                <p>皇家马德里</p>
+              </router-link>
+              </li>
             </ul>
           </div>
         </div>
@@ -218,6 +246,34 @@
                   <p>杜塞尔多夫</p>
                 </router-link>
                 </li>
+                <li><router-link to="/team/Dortmund/" tag="a">
+                  <div class="team-logo">
+                    <img :src="require('@/assets/images/Bundesliga/Dortmund.jpg')" alt=""/>
+                  </div>
+                  <p>多特蒙德</p>
+                </router-link>
+                </li>
+                <li><router-link to="/team/RBLeipzig/" tag="a">
+                  <div class="team-logo">
+                    <img :src="require('@/assets/images/Bundesliga/RBLeipzig.jpg')" alt=""/>
+                  </div>
+                  <p>莱比锡红牛</p>
+                </router-link>
+                </li>
+                <li><router-link to="/team/Bayern/" tag="a">
+                  <div class="team-logo">
+                    <img :src="require('@/assets/images/Bundesliga/Bayern.jpg')" alt=""/>
+                  </div>
+                  <p>拜仁</p>
+                </router-link>
+                </li>
+                <li><router-link to="/team/Padeborn/" tag="a">
+                  <div class="team-logo">
+                    <img :src="require('@/assets/images/Bundesliga/Paderborn.jpg')" alt=""/>
+                  </div>
+                  <p>帕德博恩</p>
+                </router-link>
+                </li>
               </ul>
             </div>
           </div>
@@ -226,16 +282,74 @@
           <div class="logo">
             <img :src="require('@/assets/images/league/serieA.png')" alt=""/>
           </div>
-          <div>
-            <p style="text-align: center;">暂无数据</p>
+          <div class="team-wrapper">
+            <ul>
+              <li><router-link to="/team/Atalanta/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/SerieA/Atalanta.jpg')" alt=""/>
+                </div>
+                <p>亚特兰大</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Brescia/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/SerieA/Brescia.jpg')" alt=""/>
+                </div>
+                <p>布雷西亚</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Napoli/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/SerieA/Napoli.jpg')" alt=""/>
+                </div>
+                <p>那不勒斯</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Juventus/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/SerieA/Juventus.jpg')" alt=""/>
+                </div>
+                <p>尤文图斯</p>
+              </router-link>
+              </li>
+            </ul>
           </div>
         </div>
         <div class="swiper-slide">
           <div class="logo">
-            <img :src="require('@/assets/images/league/Ligue1.png')" alt=""/>
+            <img :src="require('@/assets/images/league/EUROPA.jpg')" alt=""/>
           </div>
-          <div>
-            <p style="text-align: center;">暂无数据</p>
+          <div class="team-wrapper">
+            <ul>
+              <li><router-link to="/team/PSG/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Ligue1/PSG.jpg')" alt=""/>
+                </div>
+                <p>巴黎圣日耳曼</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Olympiacos/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/EUROPA/Olympiacos.png')" alt=""/>
+                </div>
+                <p>奥林匹亚科斯</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Brugge/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/EUROPA/Brugge.jpg')" alt=""/>
+                </div>
+                <p>布鲁日</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/Lyon/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/Ligue1/Lyon.jpg')" alt=""/>
+                </div>
+                <p>里昂</p>
+              </router-link>
+              </li>
+            </ul>
           </div>
         </div>
         <div class="swiper-slide">
@@ -244,6 +358,45 @@
           </div>
           <div>
             <p style="text-align: center;">暂无数据</p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="logo">
+            <img :src="require('@/assets/images/league/AFC.jpg')" alt=""/>
+          </div>
+          <div class="team-wrapper">
+            <ul>
+              <li><router-link to="/team/SydneyFC/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/AFC/SydneyFC.jpg')" alt=""/>
+                </div>
+                <p>悉尼FC</p>
+              </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="logo">
+            <img :src="require('@/assets/images/league/JLeague.png')" alt=""/>
+          </div>
+          <div class="team-wrapper">
+            <ul>
+              <li><router-link to="/team/Yokohama/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/JLeague/Yokohama.png')" alt=""/>
+                </div>
+                <p>横滨水手</p>
+              </router-link>
+              </li>
+              <li><router-link to="/team/VisselKobe/" tag="a">
+                <div class="team-logo">
+                  <img :src="require('@/assets/images/JLeague/VisselKobe.jpg')" alt=""/>
+                </div>
+                <p>神户胜利船</p>
+              </router-link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
